c2bb78c30787732481b6148fa769c9e5d42bcf78
Visall666 <viisall59@gmail.com>
2025-07-19 19:03:51 +0700

    #5 fix active and dropdown categories

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,8 @@
+  <?php
+    $settings = array(
+      'category', 'user', 'role'
+    );
+  ?>
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
@@ -29,17 +34,17 @@
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item {{in_array($active, $settings)?'menu-open':''}}">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
-                Setting
+                Settings
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('category.index')}}" class="nav-link">
+              <li class="nav-item}}" >
+                <a href="{{route('category.index')}}" class="nav-link {{$active=='category'?'active':''}}" >
                   <i class="fas fa-angle-right nav-icon"></i>
                   <p>Categories</p>
                 </a>
